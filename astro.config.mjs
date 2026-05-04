b0165ed374e766249ec2f486725b8d19c1c74fe1
GetUpCodeHub <lin706754743@gmail.com>
2026-05-04 15:23:51 +0200

    modify files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,34 +2,35 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import react from "@astrojs/react";
+import { LikeC4VitePlugin } from "likec4/vite-plugin";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://unideb-advanced-software-engineering.github.io",
   base: "/26-spring-02-greengrant",
-  integrations: [
-    starlight({
-      title: "GreenGrant",
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/unideb-advanced-software-engineering/26-spring-02-greengrant",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Requirements",
-          autogenerate: { directory: "requirements" },
-        },
-        {
-          label: "Architecture",
-          autogenerate: { directory: "architecture" },
-        },
-        {
-          label: "Decision Records",
-          autogenerate: { directory: "adrs" },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: "GreenGrant",
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/unideb-advanced-software-engineering/26-spring-02-greengrant",
+      },
+    ],
+    sidebar: [
+      {
+        label: "Requirements",
+        autogenerate: { directory: "requirements" },
+      },
+      {
+        label: "Architecture",
+        autogenerate: { directory: "architecture" },
+      },
+      {
+        label: "Decision Records",
+        autogenerate: { directory: "adrs" },
+      },
+    ],
+  }), react()],
 });
