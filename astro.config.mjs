539b8e4bc4c260b81682679f8eabc20c01961b7f
EliteC-C <chedysel23@gmail.com>
2026-05-19 14:19:19 +0200

    fixing c4

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,38 +12,38 @@ export default defineConfig({
   site: "https://unideb-advanced-software-engineering.github.io",
   base: "/26-spring-02-greengrant",
   vite: {
-    plugins: [LikeC4VitePlugin({}),],
+    plugins: [LikeC4VitePlugin({})],
   },
-  integrations: [starlight({
-    title: "GreenGrant",
-    social: [
-      {
-        icon: "github",
-        label: "GitHub",
-        href: "https://github.com/unideb-advanced-software-engineering/26-spring-02-greengrant",
-      },
-    ],
-    sidebar: [
-      {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
-      },
-      {
-        label: "Requirements",
-        autogenerate: { directory: "requirements" },
-      },
-      {
-        label: "Architecture",
-        autogenerate: { directory: "architecture" },
-      },
-      {
-        label: "Diagrams",
-        autogenerate: { directory: "diagrams" },
-      },
-      {
-        label: "Decision Records",
-        autogenerate: { directory: "adrs" },
-      },
-    ],
-  }), react(), markdoc()],
+  integrations: [
+    starlight({
+      title: "GreenGrant",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/unideb-advanced-software-engineering/26-spring-02-greengrant",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Requirements",
+          autogenerate: { directory: "requirements" },
+        },
+        {
+          label: "Architecture",
+          autogenerate: { directory: "architecture" },
+        },
+        {
+          label: "Diagrams",
+          autogenerate: { directory: "diagrams" },
+        },
+        {
+          label: "Decision Records",
+          autogenerate: { directory: "adrs" },
+        },
+      ],
+    }),
+    react(),
+    markdoc(),
+  ],
 });
